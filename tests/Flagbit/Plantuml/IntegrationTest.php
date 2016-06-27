f3dccbabfbf9f0d27ad41c2ca4aaff516baf506f
Sébastien Barbieri <seba@rtbf.be>
2016-06-28 00:48:09 +0200

    enhance integration test to support additional command parameters

--- tests/Flagbit/Plantuml/IntegrationTest.php
+++ tests/Flagbit/Plantuml/IntegrationTest.php
@@ -16,7 +16,7 @@ class IntegrationTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @dataProvider provideTestCases
+     * @dataProvider provideTestCasesNoParam
      */
     public function testIntegration($phpFile, $pumlCode)
     {
@@ -31,14 +31,42 @@ class IntegrationTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($pumlCode, $output->fetch());
     }
 
-    public function provideTestCases()
+    /**
+     * @dataProvider provideTestCasesWithGroup
+     */
+    public function testIntegrationWithGroup($phpFile, $pumlCode)
     {
+        $input = new ArrayInput(array(
+            'files' => array($phpFile),
+            '--grouping' => true
+        ));
+        $output = new BufferedOutput();
+
+        $command = new WriteCommand();
+        $command->run($input, $output);
+
+        $this->assertEquals($pumlCode, $output->fetch());
+    }
+
+    public function provideTestCasesNoParam()
+    {
+        return $this->provideTestCase('/test[a-zA-Z]*\.php$/');
+    }
+
+    public function provideTestCasesWithGroup()
+    {
+        return $this->provideTestCase('/test[a-zA-Z]*_with-group\.php$/');
+    }
+
+    private function provideTestCase($filter)
+    {
+        print($filter);
         $fixturesDir = realpath($this->getFixturesDir());
         $tests = array();
 
         foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($fixturesDir), RecursiveIteratorIterator::LEAVES_ONLY) as $file) {
             /* @var $file \SplFileInfo */
-            if (!preg_match('/\.php$/', $file)) {
+            if (!preg_match($filter, $file)) {
                 continue;
             }
 
